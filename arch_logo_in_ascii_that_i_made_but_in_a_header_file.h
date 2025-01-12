ce0af6a4ffcf78ebdba82e52496bf831cb28b936
olight0 <157029034+olight0@users.noreply.github.com>
2025-01-12 18:31:09 +0300

    Create arch_logo_in_ascii_that_i_made_but_in_a_header_file.h

---
+++ arch_logo_in_ascii_that_i_made_but_in_a_header_file.h
@@ -0,0 +1,28 @@
+#ifndef LOGOS_H
+#define LOGOS_H
+
+#include <stdio.h>
+
+void archlinux() {
+    printf("\033[94m");
+    printf("             -                                                    \n");
+    printf("            /+\\                                                  \n");
+    printf("           /+++\\                                                 \n");
+    printf("          /+++++\\                                                \n");
+    printf("         /+++++++\\                                               \n");
+    printf("        /+++++++++\\                                              \n");
+    printf("       /+++++++++++\\                                             \n");
+    printf("      /+++++++++++++\\                                            \n");
+    printf("\e[0;34m");
+    printf("     /+++++++++++++++\\                                           \n");
+    printf("    /+++++++++++++++++\\                                          \n");
+    printf("   /+++++++     +++++++\\                                         \n");
+    printf("  /+++++++       +++++++\\                                        \n");
+    printf(" /+++++++         +++++++\\                                       \n");
+    printf("/+++++++           +++++++\\                                      \n");
+    printf("                                                                 \n");
+    printf("                                                                 \n");
+    printf("\033[0m");
+}
+
+#endif // LOGOS_H
